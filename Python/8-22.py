aef1e7f568fc41507e10f0d9f646543b250d65f3
will <willbell0@icloud.com>
2019-08-22 14:48:19 -0400

    8/22

---
+++ Python/8-22.py
@@ -0,0 +1,22 @@
+
+#Given a String return a new string with opposite case and order ex("AbcD ss"==>"SS aBCd")
+#https://www.codewars.com/kata/string-transformer/
+def string_transformer(s):
+    s=s.split(' ')
+    main=''
+    ret=''
+    for x in range(len(s)):
+        strs=''
+        for y in range(len(s[x])):
+            if(s[x][y].isupper()):
+                strs+=s[x][y].lower()
+            else:
+                strs+=s[x][y].upper()
+        main+=strs
+        main+=' '
+    ret=main.split(' ')
+    ret.reverse()
+
+    return ' '.join(ret)[1:]
+
+print(string_transformer("Example string weiner poopY"))
