57fa2845786be20074b1caf133851ac40bf4b7c4
will <willbell0@icloud.com>
2020-03-02 17:04:04 -0500

    3/2/20

---
+++ Python/3-2.py
@@ -0,0 +1,34 @@
+class Vector(object):
+    def __init__(self):
+       self.val=[]
+       self.length=len(self.val)
+    def __init__(self,val):
+        self.val = val
+        self.length = len(self.val)
+    def add(self,vec):
+        if self.length != vec.length:
+            raise Exception
+        return Vector([i + j for i,j in zip(self.val,vec.val)])
+    def subtract(self,vec):
+        if self.length != vec.length:
+            raise Exception
+        return Vector([i - j for i,j in zip(self.val,vec.val)])
+    def dot(self,vec):
+        if self.length != vec.length:
+            raise Exception
+        return sum([i * j for i,j in zip(self.val,vec.val)])
+    def norm(self):
+        return (sum([i**2 for i in self.val]))**.5
+    def __str__(self):
+        return "(" + str((self.val))[1:len(str(self.val))-1] + ")"
+    def equals(self,vec):
+        if vec == None:
+            return False
+        return self.val == vec.val
+
+
+
+x = Vector([1,2])
+print(x.val)
+print(x.norm())
+print(str(x))
